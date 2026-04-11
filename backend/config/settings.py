8e690fc7c0d2f323bed17174aefdfa27da0d22be
Mahefa-MaH <mahefarandrianavalona@gmail.com>
2026-04-11 09:53:47 +0300

    update ALLOWED_HOSTS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4j)a%^42-4@q@-xd&o*h0w#!64=edn62yb^($^(drkz%&a_dh5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
